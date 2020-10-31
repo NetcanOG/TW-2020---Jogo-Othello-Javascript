49d5a79959b1338e9569d0bcf9e22e6067babe89
Miguel Ramos <netcan@snaff.khasa.net>
2020-10-31 18:04:25 +0000

    ASDF

---
+++ README.md
@@ -0,0 +1,15 @@
+O QUE FALTA FAZER
+
+Patrícia:
+-Botão voltar nas Cores (Cores para Dificuldade)
+-Alterar CSS Regras
+-Fazer CSS Points
+-Ver melhor CSS Resultados
+-Avisa de troca de vez
+
+Miguel:
+-Traduzir Regras
+XXXX Fazer funçáo para as vitorias
+XXXX Fazer função Reset Board  
+XXXX Modo Fácil
+XXXX Modo Dificil
--- js/settings.js
+++ js/settings.js
@@ -1,61 +1,49 @@
 "use strict";
-function showbegin(){
-    document.getElementById("spage").style.display= "block";
-    document.getElementById("loginform").style.display= "none";
-    document.getElementById("header").style.display= "none";
-    document.getElementById("modebutton").style.display= "none";
-    document.getElementById("topnav").style.display= "none";
-    document.getElementById("difficulty").style.display= "none";
-    document.getElementById("game").style.display= "none";
-    document.getElementById("rules").style.display= "none";
+
+function showbegin() {
+    location.reload();
 }
 
-function showlogin(){
-    document.getElementById("spage").style.display= "none";
-    document.getElementById("header").style.display= "block";
-    document.getElementById("loginform").style.display= "block";
+function showlogin() {
+    document.getElementById("spage").style.display = "none";
+    document.getElementById("header").style.display = "block";
+    document.getElementById("loginform").style.display = "block";
 }
 
-function showmode(){
-    document.getElementById("loginform").style.display= "none";
-    document.getElementById("topnav").style.display= "block";
-    document.getElementById("voltarmodo").style.display= "none";
-    document.getElementById("voltarregras").style.display= "none";
-    document.getElementById("modebutton").style.display= "block";
-    document.getElementById("difficulty").style.display= "none";  
-    document.getElementById("rules").style.display= "none";
+function showmode() {
+    document.getElementById("loginform").style.display = "none";
+    document.getElementById("voltarmodo").style.display = "none";
+    document.getElementById("voltarregras").style.display = "none";
+    document.getElementById("modebutton").style.display = "block";
+    document.getElementById("difficulty").style.display = "none";
 }
 
-function showdif(bot){
-    document.getElementById("modebutton").style.display= "none";
-    document.getElementById("voltarmodo").style.display= "block";
-    document.getElementById("difficulty").style.display= "block";
+function showdif(bot) {
+    document.getElementById("modebutton").style.display = "none";
+    document.getElementById("voltarmodo").style.display = "block";
+    document.getElementById("difficulty").style.display = "block";
     vsBot = bot;
 }
-function showcolor(diff){
-    document.getElementById("color").style.display= "block";
-    document.getElementById("voltarmodo").style.display= "none";
-    document.getElementById("difficulty").style.display= "none";
+
+function showcolor(diff) {
+    document.getElementById("color").style.display = "block";
+    document.getElementById("voltarmodo").style.display = "none";
+    document.getElementById("difficulty").style.display = "none";
     botDifficulty = diff;
 }
 
-function showregras(){
-    document.getElementById("modebutton").style.display= "none";
-    document.getElementById("voltarregras").style.display= "block";
-    document.getElementById("rules").style.display= "block";
-}
-
-function showgame(color){
-    document.getElementById("voltarmodo").style.display= "none";
-    document.getElementById("voltarregras").style.display= "none";
-    document.getElementById("modebutton").style.display= "none";
-    document.getElementById("color").style.display= "none";
-    document.getElementById("game").style.display= "block";
+function showgame(color) {
+    document.getElementById("header").style.display = "none";
+    document.getElementById("voltarmodo").style.display = "none";
+    document.getElementById("headerjogo").style.display = "block";
+    document.getElementById("modebutton").style.display = "none";
+    document.getElementById("color").style.display = "none";
+    document.getElementById("game").style.display = "block";
     playerColor = color;
-    if(playerColor == 1){
-      botColor = 2;
-    } else if(playerColor == 2){
-      botColor = 1;
+    if (playerColor == 1) {
+        botColor = 2;
+    } else if (playerColor == 2) {
+        botColor = 1;
     }
     createGrid();
 }
