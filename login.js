db6b44abf2369c4f5a2d2c8bdaa7468e2d404364
Patrícia Vieira <54964992+PatriciaV24@users.noreply.github.com>
2020-11-02 15:02:27 +0000

    Versão Final - 94%

---
+++ login.js
@@ -0,0 +1,77 @@
+/*FORM LOGIN*/
+const inputs = document.querySelectorAll(".input");
+
+function addcl() {
+    let parent = this.parentNode.parentNode;
+    parent.classList.add("focus");
+}
+
+function remcl() {
+    let parent = this.parentNode.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus");
+    }
+}
+
+
+inputs.forEach(input => {
+    input.addEventListener("focus", addcl);
+    input.addEventListener("blur", remcl);
+});
+
+/*POP UP*/
+/*Regras */
+
+function showregras() {
+    document.getElementById("popregras").style.display = "block";
+}
+
+function closeregras() {
+    document.getElementById("popregras").style.display = "none";
+}
+
+/*Resultado */
+function showresultado() {
+    document.getElementById("popvitorias").style.display = "block";
+}
+
+function closeresultado() {
+    document.getElementById("popvitorias").style.display = "none";
+}
+
+/*Ganhou */
+function showpopganhou(player) {
+    document.getElementById("popturn").style.display = "none";
+    switch (player) {
+        case 1:
+            document.getElementById("popdesistiu").style.display = "block";
+            document.getElementById("legenda1").innerHTML = "Black forfeits, White Won!";
+            break;
+        case 2:
+            document.getElementById("popdesistiu").style.display = "block";
+            document.getElementById("legenda1").innerHTML = "White forfeits, Black Won!";
+            break;
+        case 3:
+            document.getElementById("popganhou").style.display = "block";
+            document.getElementById("legenda").innerHTML = "Game over, Black Won!";
+            break;
+        case 4:
+            document.getElementById("popganhou").style.display = "block";
+            document.getElementById("legenda").innerHTML = "Game over, White Won!";
+            break;
+        case 5:
+            document.getElementById("popganhou").style.display = "block";
+            document.getElementById("legenda").innerHTML = "End of the game, it's a tie!!";
+            break;
+    }
+    document.getElementById("desistir").style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById("popregras")) {
+        document.getElementById("popregras").style.display = "none";
+    }
+    if (event.target == document.getElementById("popvitorias")) {
+        document.getElementById("popvitorias").style.display = "none";
+    }
+}
---
+++ settings.js
@@ -0,0 +1,46 @@
+"use strict";
+
+function showbegin() {
+    location.reload();
+}
+
+function showlogin() {
+    document.getElementById("spage").style.display = "none";
+    document.getElementById("header").style.display = "block";
+    document.getElementById("loginform").style.display = "block";
+
+}
+
+function showmode() {
+    document.getElementById("loginform").style.display = "none";
+    document.getElementById("modebutton").style.display = "block";
+    document.getElementById("difficulty").style.display = "none";
+}
+
+function showdif(bot) {
+    document.getElementById("modebutton").style.display = "none";
+    document.getElementById("difficulty").style.display = "block";
+    document.getElementById("color").style.display = "none";
+    vsBot = bot;
+}
+
+function showcolor(diff) {
+    document.getElementById("color").style.display = "block";
+    document.getElementById("difficulty").style.display = "none";
+    botDifficulty = diff;
+}
+
+function showgame(color) {
+    document.getElementById("header").style.display = "none";
+    document.getElementById("headerjogo").style.display = "block";
+    document.getElementById("modebutton").style.display = "none";
+    document.getElementById("color").style.display = "none";
+    document.getElementById("game").style.display = "block";
+    playerColor = color;
+    if (playerColor == 1) {
+        botColor = 2;
+    } else if (playerColor == 2) {
+        botColor = 1;
+    }
+    createGrid();
+}
