fcbeb9b36548c036e70d847146222d73ef11a420
Miguel Ramos <netcan@snaff.khasa.net>
2020-11-29 18:16:25 +0000

    register working

--- onlinegame.js
+++ onlinegame.js
@@ -1,3 +1,31 @@
 "use strict";
 
 var url = "http://twserver.alunos.dcc.fc.up.pt:8008/";
+var nick;
+var pass;
+var roomid = "5242";
+
+function register(){
+
+ nick = document.getElementById("username").value;
+ pass = document.getElementById("password").value;
+
+ console.log("username = "+nick);
+ console.log("password = "+pass);
+
+ fetch(url + "register",{
+   method: "POST",
+   body: JSON.stringify({
+     "nick": nick,
+     "pass": pass
+   })
+ }).then(response => {
+   if(response.ok){
+     console.log(response);
+     showmode();
+   }
+   else{
+     alert("Wrong password");
+   }
+ })
+}
