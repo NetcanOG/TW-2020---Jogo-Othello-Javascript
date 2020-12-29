e1181a44cca373c1a6d8cf93ff62f8abdbab7be3
PatriciaV24 <pdtvieira@hotmail.com>
2020-12-29 16:09:38 +0000

    dd

---
+++ heads.js
@@ -0,0 +1,33 @@
+const headers = {
+    plain: {
+        'Content-Type': 'application/javascript',
+        'Cache-Control': 'no-cache',
+        'Access-Control-Allow-Origin': '*'        
+    },
+    sse: {    
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Access-Control-Allow-Origin': '*',
+        'Connection': 'keep-alive'
+    },
+    txt: {
+        'Content-Type': 'text/plain',
+        'Cache-Control': 'no-cache',
+        'Access-Control-Allow-Origin': '*'
+    },
+    html:{
+        'Content-Type': 'text/html',
+        'Cache-Control': 'no-cache',
+        'Access-Control-Allow-Origin': '*',
+    },
+    css: {
+        'Content-Type': 'text/css',
+        'Cache-Control': 'no-cache',
+        'Access-Control-Allow-Origin': '*',
+    }, 
+    png:{
+        'Content-Type': 'image/png',
+        'Cache-Control': 'no-cache',
+        'Access-Control-Allow-Origin': '*',
+    }
+};
