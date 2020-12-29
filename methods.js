9cd090b82c46db3b12deced513405217681c0969
Miguel Ramos <netcan@snaff.khasa.net>
2020-12-29 01:40:32 +0000

    geg

--- methods.js
+++ methods.js
@@ -1,5 +1,7 @@
+const crypto = require('crypto');
+var userlist = [];
+
 module.exports.postRequest = function(req, res, path, query){  
-  console.log("pathname: "+path);
   switch(path){
  
     case('/ranking'):
@@ -13,7 +15,49 @@ module.exports.postRequest = function(req, res, path, query){
 }
 
 function register(response, nick, pass){
+  
+  const hash = crypto
+               .createHash('md5')
+               .update(pass)
+               .digest('hex');
 
-  console.log("nickname: "+nick+"  password: "+pass);
+  fs.readFile('users.json', 'utf8', (err,data) => {
+
+    if(err){
+      response.writeHead(400, {"Content-Length": body.length, "Content-Type": "text/plain"});
+      response.write('{ "error": "Bad Request" }');
+      response.end();
+      return;
+    }else{
+      for(var i = 0; i < userlist.length; i++){
+        if(userlist[i].nick == nick && userlist[i].pass == hash){
+          response.writeHead(200, {"Content-Length": body.length, "Content-Type": "text/plain"});
+          response.write('{}');
+          response.end();
+          return;
+        }
+        if(userlist[i].nick == nick && userlist[i].pass != hash){
+          response.writeHead(401, {"Content-Length": body.length, "Content-Type": "text/plain"});
+          response.write('{ "error": "Wrong password" }');
+          response.end();
+          return;
+        }
+      }
+      addUser(nick, hash);
+      response.writeHead(200, {"Content-Length": body.length, "Content-Type": "text/plain"});
+      response.write('{}');
+      response.end();
+    }
+
+  })
+
+  console.log("nickname: "+nick+"  password: "+pass+"  hashed password: "+hash);
   //console.log(response);
+}
+
+function addUser(nick, hash){
+  const user = {"nick": nick,
+                "pass": hash};    
+  userlist.push(user);
+  fs.writeFile('./users.json', JSON.stringify(userlist),(err) => {if(err) console.error(err);})
 }
